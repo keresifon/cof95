b0ecaa0fa91decf396c9c3ae4c2147b28f1bce23
Keresifon <keresifon@gmail.com>
2021-07-11 13:09:13 +0100

    Update sign out to redirect to home

--- src/component/SignOut.js
+++ src/component/SignOut.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Auth} from "aws-amplify";
+import { useHistory } from 'react-router-dom';
+
 
 function SignOut(props) {
+    const history = useHistory();
+
     const signOut = (e) => {
         e.preventDefault();
         Auth.signOut();
-        window.location.reload() 
-      }
+        window.location.reload();
+        history.push("/");
+    }
     return (
       <button onClick={signOut} className="btn btn-lg btn-outline-primary rounded-pill">
         Sign out
